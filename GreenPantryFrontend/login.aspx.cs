cf505d0021b55faf066238a99c3bb7fb090b3594
Ibrahim Chhaya <16260791+DJ-DK786@users.noreply.github.com>
2020-08-14 21:47:15 +0200

    Add project files.

---
+++ GreenPantryFrontend/login.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GreenPantryFrontend
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void login_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
