38eb53afdf4adbb97d2684bd64855470b6b630d2
TenilleF19 <tjsforbes@gmail.com>
2020-10-06 22:14:25 +0200

    Removed a comment

--- GreenPantryFrontend/dashboard/dashboard.aspx.cs
+++ GreenPantryFrontend/dashboard/dashboard.aspx.cs
@@ -154,29 +154,6 @@ namespace AdminDashboard
                 Display += "<td><i class='fas fa-arrow-up text-success mr-3'></i> dunno%</td></tr>";
             }
             pageTraffic.InnerHtml = Display;
-
-
-
-
-
-            //foreach(var c in categories)
-            //{
-            //    var profit = SR.profitPerCat(c.ID);
-            //    profits.Add(profit);
-            //}" + categories[3].Name + "
-
-            //display += "<ul class='nav nav-pills justify-content-end'>";
-            //display += "<li class='nav-item mr-2 mr-md-0' data-toggle='chart' data-target='#chart-sales-dark'";
-            //display += "data-update='{\"data\":{\"labels\":[\"" + categories[0].Name + "\",\"" + categories[1].Name + "\",\"" + categories[2].Name + "\",\"" + categories[3].Name + "\",\"" + categories[4].Name + "\",\"" + categories[5].Name + "\",\"" + categories[6].Name + "\",\"" + categories[7].Name + "\"],\"datasets\":[{\"data\":[0,20,10,30,15,40,20,60,60]}]}}' data-prefix='R' data-suffix='k'>";
-            //display += "<a href='#' class='nav-link py-2 px-3 active' data-toggle='tab'>";
-            //display += "<span class='d-none d-md-block'>Month</span><span class='d-md-none'>M</span></a></li>";
-            //display += "<li class='nav-item' data-toggle='chart' data-target='#chart-sales-dark' data-update='{\"data\":{\"labels\":[\"This\",\"Is\",\"Going\",\"be\",\"The\",\"Death\",\"Of\",\"me\"],";
-            //display += "\"datasets\":[{\"data\":[0, 20, 5, 25, 10, 30, 15, 40, 40]}]}}' data-prefix='R' data-suffix='k'>";
-            //display += "<a href='#' class='nav-link py-2 px-3' data-toggle='tab'>";
-            //display += "<span class='d-none d-md-block'>Week</span><span class='d-md-none'>W</span></a></li> </ul>";
-
-            //chart1Li.InnerHtml = display;
-
         }
     }
 }
