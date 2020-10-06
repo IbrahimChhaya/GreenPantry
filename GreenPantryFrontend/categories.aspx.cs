090e5d5e61d10655de0e7143677ca0ffb4666903
TenilleF19 <tjsforbes@gmail.com>
2020-10-06 22:19:56 +0200

    Price filtering for Categories and sub-categories

--- GreenPantryFrontend/categories.aspx.cs
+++ GreenPantryFrontend/categories.aspx.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,56 +13,76 @@ namespace GreenPantryFrontend
     public partial class categories : System.Web.UI.Page
     {
         GP_ServiceClient SC = new GP_ServiceClient();
+
+        public string jsonProducts;
         protected void Page_Load(object sender, EventArgs e)
         {
-            String display = "";
-
-            String catID = Request.QueryString["CategoryID"];
-
-            dynamic category = SC.getCat(int.Parse(catID));
-
-            display += "<h2>" + category.Name + "</h2>";
-            display += "<div class='breadcrumb__option'>";
-            display +=  "<a href='./home.aspx'>Home</a>";
-            display += "<span>" + category.Name + "</span></div>";
-
-            breadcrumb.InnerHtml = display;
-
-            display = "";
-            dynamic subcats = SC.getSubCatPerCat(int.Parse(catID));
-            //int numProducts = SC.getNumProductsInSub(subcats.SubID);
-            foreach(SubCategory sc in subcats)
-            { 
-                display += "<li><a href='/subcategory.aspx?SubcategoryID=" + sc.SubID + "'>" + sc.Name + "</a></li>";
-            }
-            subcatList.InnerHtml = display;
-
-            display = "";
-            decimal high = 0;
-            dynamic products = SC.getProductByCat(int.Parse(catID));
-            foreach(Product p in products)
+            if(Request.QueryString["CategoryID"] == null)
             {
-                display += "<div class='col-lg-4 col-md-6 col-sm-6'>";
-                display += "<div class='product__item' onclick='location.href=&#39;singleproduct.aspx?ProductID=" + p.ID + "&#39;'>";
-                display += "<div class='product__item__pic set-bg' data-setbg='" + p.Image_Location + "'>";
-                display += "<ul class='product__item__pic__hover'>";
-                display += "<li><a href='#'><i class='fa fa-heart'></i></a></li>";
-                display += "<li><a href='#'><i class='fa fa-shopping-cart'></i></a></li></ul></div>";
-                display += "<div class='product__item__text'>";
-                display += "<h6>" + p.Name + "</h6>";
-                display += "<h5>R" + Math.Round(p.Price,2) + "</h5></div></div></div>";
-                if (p.Price > high)
-                    high = p.Price;
+                Response.Redirect("home.aspx");
             }
-            categoryProducts.InnerHtml = display;
+            else { 
+                String display = "";
 
-            display = "";
-            display += "<div class='price-range ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content'";
-            display += "data-min='0' data-max='"+ high + "'>";
-            display += "<div class='ui-slider-range ui-corner-all ui-widget-header'></div>";
-            display += "<span tabindex='0' class='ui-slider-handle ui-corner-all ui-state-default'></span>";
-            display += "<span tabindex='0' class='ui-slider-handle ui-corner-all ui-state-default'></span></div>";
-            //maxPrice.InnerHtml = display;
+                String catID = Request.QueryString["CategoryID"];
+
+                dynamic category = SC.getCat(int.Parse(catID));
+
+                display += "<h2>" + category.Name + "</h2>";
+                display += "<div class='breadcrumb__option'>";
+                display +=  "<a href='./home.aspx'>Home</a>";
+                display += "<span>" + category.Name + "</span></div>";
+
+                breadcrumb.InnerHtml = display;
+
+                display = "";
+                dynamic subcats = SC.getSubCatPerCat(int.Parse(catID));
+                //int numProducts = SC.getNumProductsInSub(subcats.SubID);
+                foreach(SubCategory sc in subcats)
+                { 
+                    display += "<li><a href='/subcategory.aspx?SubcategoryID=" + sc.SubID + "'>" + sc.Name + "</a></li>";
+                }
+                subcatList.InnerHtml = display;
+
+                display = "";
+                decimal high = 0;
+                dynamic products = SC.getProductByCat(int.Parse(catID));
+            
+
+                foreach(Product p in products)
+                {
+                    display += "<div class='col-lg-4 col-md-6 col-sm-6'>";
+                    display += "<div class='product__item' onclick='location.href=&#39;singleproduct.aspx?ProductID=" + p.ID + "&#39;'>";
+                    display += "<div class='product__item__pic set-bg' data-setbg='" + p.Image_Location + "'>";
+                    display += "<ul class='product__item__pic__hover'>";
+                    display += "<li><a href='#'><i class='fa fa-heart'></i></a></li>";
+                    display += "<li><a href='#'><i class='fa fa-shopping-cart'></i></a></li></ul></div>";
+                    display += "<div class='product__item__text'>";
+                    display += "<h6>" + p.Name + "</h6>";
+                    display += "<h5>R" + Math.Round(p.Price,2) + "</h5></div></div></div>";
+                }
+                categoryProducts.InnerHtml = display;
+
+                display = "";
+                display += "<div class='price-range ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content'";
+                display += "data-min='0' data-max='"+ high + "'>";
+                display += "<div class='ui-slider-range ui-corner-all ui-widget-header'></div>";
+                display += "<span tabindex='0' class='ui-slider-handle ui-corner-all ui-state-default'></span>";
+                display += "<span tabindex='0' class='ui-slider-handle ui-corner-all ui-state-default'></span></div>";
+
+            }
+        }
+
+        //function to get all products
+        public string getProducts()
+        {
+            Product[] products = SC.getProductByCat(Convert.ToInt32(Request.QueryString["CategoryID"]));
+            //create js serialized object to pass to js
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            jsonProducts = serializer.Serialize(products);
+            //jsonProducts = products;
+            return jsonProducts;
+
         }
 
         private void saveToCookie(String CookieName, String content)
--- GreenPantryFrontend/subcategory.aspx.cs
+++ GreenPantryFrontend/subcategory.aspx.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -12,47 +13,67 @@ namespace GreenPantryFrontend
     {
         GP_ServiceClient SC = new GP_ServiceClient();
 
+        public string jsonProducts;
         protected void Page_Load(object sender, EventArgs e)
         {
-            String display = "";
-
-            String subID = Request.QueryString["SubcategoryID"];
-
-            dynamic subcat = SC.getSubCat(int.Parse(subID));
-            
-            //breadcrumb-------------------------------------------------------------
-
-            display += "<h2>" + subcat.Name + "</h2>";
-            display += "<div class='breadcrumb__option'>";
-            display += "<a href='./home.aspx'>Home</a>";
-            display += "<span>" + subcat.Name + "</span></div>";
-
-            breadcrumb.InnerHtml = display;
-            
-            //filterd by sidebar-------------------------------------------------------------
-
-            display = "";
-            display += "<li>" + subcat.Name + "</li>";
-            
-            filtered.InnerHtml = display;
-
-            //products-------------------------------------------------------------
-
-            display = "";
-            dynamic products = SC.getProductBySubCat(int.Parse(subID));
-            foreach (Product p in products)
+            if(Request.QueryString["SubcategoryID"] == null)
             {
-                display += "<div class='col-lg-4 col-md-6 col-sm-6'>";
-                display += "<div class='product__item' onclick='location.href=&#39;singleproduct.aspx?ProductID=" + p.ID + "&#39;'>";
-                display += "<div class='product__item__pic set-bg' data-setbg='" + p.Image_Location + "'>";
-                display += "<ul class='product__item__pic__hover'>";
-                display += "<li><a href='#'><i class='fa fa-heart'></i></a></li>";
-                display += "<li><a href='#'><i class='fa fa-shopping-cart'></i></a></li></ul></div>";
-                display += "<div class='product__item__text'>";
-                display += "<h6>" + p.Name + "</h6>";
-                display += "<h5>R" + Math.Round(p.Price, 2) + "</h5></div></div></div>";
+                Response.Redirect("home.aspx");
             }
-            subProducts.InnerHtml = display;
+            else
+            {
+                String display = "";
+
+                String subID = Request.QueryString["SubcategoryID"];
+
+                dynamic subcat = SC.getSubCat(int.Parse(subID));
+
+                //breadcrumb-------------------------------------------------------------
+
+                display += "<h2>" + subcat.Name + "</h2>";
+                display += "<div class='breadcrumb__option'>";
+                display += "<a href='./home.aspx'>Home</a>";
+                display += "<span>" + subcat.Name + "</span></div>";
+
+                breadcrumb.InnerHtml = display;
+
+                //filterd by sidebar-------------------------------------------------------------
+
+                display = "";
+                display += "<li>" + subcat.Name + "</li>";
+
+                filtered.InnerHtml = display;
+
+                //products-------------------------------------------------------------
+
+                display = "";
+                dynamic products = SC.getProductBySubCat(int.Parse(subID));
+                foreach (Product p in products)
+                {
+                    display += "<div class='col-lg-4 col-md-6 col-sm-6'>";
+                    display += "<div class='product__item' onclick='location.href=&#39;singleproduct.aspx?ProductID=" + p.ID + "&#39;'>";
+                    display += "<div class='product__item__pic set-bg' data-setbg='" + p.Image_Location + "'>";
+                    display += "<ul class='product__item__pic__hover'>";
+                    display += "<li><a href='#'><i class='fa fa-heart'></i></a></li>";
+                    display += "<li><a href='#'><i class='fa fa-shopping-cart'></i></a></li></ul></div>";
+                    display += "<div class='product__item__text'>";
+                    display += "<h6>" + p.Name + "</h6>";
+                    display += "<h5>R" + Math.Round(p.Price, 2) + "</h5></div></div></div>";
+                }
+                subProducts.InnerHtml = display;
+            }
+            
+        }
+
+        public string getProducts()
+        {
+            Product[] products = SC.getProductBySubCat(Convert.ToInt32(Request.QueryString["SubcategoryID"]));
+            //create js serialized object to pass to js
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            jsonProducts = serializer.Serialize(products);
+            //jsonProducts = products;
+            return jsonProducts;
+
         }
     }
 }
