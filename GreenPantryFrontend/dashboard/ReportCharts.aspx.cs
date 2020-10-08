326dfef346eac90369b99cbd61ebcc8cc561868f
Zeerak Muhammad Baig <zmbaig98@gmail.com>
2020-10-08 21:56:33 +0200

    Report Charts added

--- GreenPantryFrontend/dashboard/ReportCharts.aspx.cs
+++ GreenPantryFrontend/dashboard/ReportCharts.aspx.cs
@@ -7,25 +7,29 @@ using System.Web.Script.Serialization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace AdminDashboard
+namespace GreenPantryFrontend.dashboard
 {
-    public partial class reports : System.Web.UI.Page
+    public partial class ReportCharts : System.Web.UI.Page
     {
-
         GP_ServiceClient SR = new GP_ServiceClient();
         public string catChart;
+        public string catName;
         protected void Page_Load(object sender, EventArgs e)
         {
-            List<ProductCategory> cat = new List<ProductCategory>();
+            List<int> cat = new List<int>();
+            List<String> categories = new List<String>();
             dynamic category = SR.getAllCategories();
-            foreach(ProductCategory pc in category)
+            foreach (ProductCategory pc in category)
             {
-                cat.Add(pc);
+                cat.Add(pc.ID);
+                categories.Add(pc.Name);
+
             }
 
             //creating a javascript serializer
             JavaScriptSerializer jSerializer = new JavaScriptSerializer();
             catChart = jSerializer.Serialize(cat);
+            catName = jSerializer.Serialize(categories);
 
         }
     }
